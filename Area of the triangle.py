2b39f26a3c56d57cb97c9fbb37da51bf146b912a
Divyendra-pro <64890062+Divyendra-pro@users.noreply.github.com>
2020-07-29 18:38:55 +0530

    Create Area of the triangle.py
    This is the simple python program which will Calculate the area of the triangle

---
+++ Area of the triangle.py
@@ -0,0 +1,13 @@
+#Python program to calculate the area of the triangle
+
+#User input for the lenght of the triangle
+l=input('Enter the lenght of the triangle: ')
+
+#User input for the breadth of the triangle
+b=input('Enter the breadth of the tiangle: ')
+
+#Formula of the triagnle
+ar=2*l+b
+
+#The final result
+print('Area of the triangle: ',ar)
